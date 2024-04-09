1232f1bd7d7e36dbdf08cdff0458e32307189d09
Alexander Oh <oh.a@gmx.at>
2024-04-09 14:08:28 +0200

    add iam data source

--- src/main/java/scalable/monster/demo/DemoApplication.java
+++ src/main/java/scalable/monster/demo/DemoApplication.java
@@ -2,9 +2,17 @@ package scalable.monster.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
+
+
+  // uncomment if you need normal postgres support
+  @Bean
+  public IamDatasource iamDatasource() {
+    return new IamDatasource();
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/java/scalable/monster/demo/IamDatasource.java
@@ -0,0 +1,37 @@
+package scalable.monster.demo;
+
+import com.zaxxer.hikari.HikariDataSource;
+import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
+import software.amazon.awssdk.services.rds.RdsClient;
+import software.amazon.awssdk.services.rds.RdsUtilities;
+import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
+
+import java.net.URI;
+
+public class IamDatasource extends HikariDataSource {
+  @Override
+  public String getPassword() {
+    // Alternatively, you can create a Bean declaration of RdsClient. For demo purposes, I have decided to instantiate it here.
+    RdsClient rdsClient = RdsClient.builder()
+      .region(new DefaultAwsRegionProviderChain().getRegion())
+      .build();
+
+    RdsUtilities rdsUtilities = rdsClient.utilities();
+
+    URI jdbcUri = parseJdbcURL(getJdbcUrl());
+
+    GenerateAuthenticationTokenRequest request = GenerateAuthenticationTokenRequest.builder()
+      .username(getUsername())
+      .hostname(jdbcUri.getHost())
+      .port(jdbcUri.getPort())
+      .build();
+
+    return rdsUtilities.generateAuthenticationToken(request);
+  }
+
+  private URI parseJdbcURL(String jdbcUrl) {
+    String uri = jdbcUrl.substring(5);
+    return URI.create(uri);
+  }
+}
+
